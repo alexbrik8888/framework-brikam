5a6d8906df0386a9ca8c416d383eb3c7fe3e02de
alexbrik8888 <108339952+alexbrik8888@users.noreply.github.com>
2026-07-31 20:23:23 +0300

    fix

--- App/Engine/DB.php
+++ App/Engine/DB.php
@@ -2,10 +2,12 @@
 
 namespace App\Engine;
 
+use PDOStatement;
+
 class DbExceptionSQL {
     public  $sql = '';
     public function __construct($sql) {
-        $this->sql = $sql;
+        $this->sql =   addslashes($sql);
     }
 }
 
@@ -29,18 +31,18 @@ class DB {
     protected $strQusery = '';
 
     public function __construct() {
-            $this->config = Cache::getInstance()->get('db');
+            $this->config = Config::getInstance()->getConfig('db');
             $dsn = sprintf("mysql:host=%s;dbname=%s;charset=%s",
                 $this->config['host'],
                 $this->config['dbname'],
                 $this->config['charset'] ?? 'utf8mb4'
             );
         $options = [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES   => false,
+            \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+            \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+            \PDO::ATTR_EMULATE_PREPARES   => false,
         ];
-        self::$pdo = new PDO($dsn, $this->config['user'], $this->config['pass'], $options);
+        self::$pdo = new \PDO($dsn, $this->config['user'], $this->config['pass'], $options);
      }
     public function __destruct() {
         self::$pdo = null;
@@ -63,7 +65,7 @@ class DB {
         );
     }
 
-    public static function raw($sql) {return new DbExceptionSQL($sql);}
+    public static function raw($sql):DbExceptionSQL {return new DbExceptionSQL($sql);}
 
     public function select(string|array $select): self{
         if(is_string($select))
@@ -73,16 +75,16 @@ class DB {
         return $this;
     }
 
-    public function from( string|array $from){
+    public function from( string|array $from) :self{
         if(is_string($from))
-            $this->select[] = $from;
+            $this->from[] = $from;
         if(is_array($from))
-            $this->select[] =array_merge($this->from,$from);
+            $this->from[] =array_merge($this->from,$from);
         return $this;
     }
 
 
-    protected function join(string $table, string|array $on = '', string $type = 'INNER') {
+    protected function join(string $table, string|array $on = '', string $type = 'INNER') :self {
         if(!empty($table))
             throw new \Exception('table empty');
         if(!empty($on)) {
@@ -96,7 +98,6 @@ class DB {
                 } else {
                     throw new \Exception("Формат масива не верен [['field'=>'','type'=>'','operator'=>'','value'=>''],...]");
                 }
-
             }
             $this->join[] = sprintf('%s JOIN  %s ON %s ',$type, $table, $on);
             return $this;
@@ -126,111 +127,234 @@ class DB {
         return $this->join($table,$where,'INNER');
     }
 
-    public function where( string $field, mixed $value, string $operator = '=', string $type = 'and'){
-            $this->where[] = sprintf(" %s %s %s '%s' ",(count($this->where) > 1) ? $type : '', $field, $operator, $value );
+    protected function where_( string $field, mixed $value, string $operator = '=', string $type = 'and') :string{
+        $type = strtoupper($type);
+        $prefix = empty($this->where) ? '' : "{$type} ";
+        $paramName = ':' . str_replace('.', '_', $field) . '_' . count($this->bindings);
+        $this->bindings[$paramName] = $value;
+        return "{$prefix}{$field} {$operator} {$paramName}";
     }
-    public function whereMultiple($whereArr){
+
+    public function where(string $field, mixed $value, string $operator = '=', string $type = 'and') :self {
+        $this->where[] = $this->where_( $field,$value,$operator,$type);
+        return $this;
+    }
+    public function whereMultiple( array $whereArr) :self {
         if(is_array($whereArr) && array_diff_key($whereArr[0],['field'=>1,'value'=>1,'operator'=>1,'type']) ==  0) {
             foreach($whereArr as $where){
-                $this->where[] =  sprintf(" %s %s %s '%s' " ,
-                    (count($whereArr) > 1)?$where['type']:'',$where['field'], $where['value'], $where['operator']);
+                $this->where[] = $this->where_( $where['field'],$where['value'],$where['operator'],$where['type']);
             }
+        } else {
+            throw new \Exception("Формат масива не верен [['field'=>'','type'=>'','operator'=>'','value'=>''],...]");
         }
         return $this;
     }
 
-    public function whereIN($field,$value,$type = 'and') {
-            if(is_array($value) && is_string($field)) {
-                $this->where[] =  sprintf(" %s %s IN ( %s ) ", (count($this->where) > 1) ? $type : '',$field, implode(',',$value));
-            }
+    public function whereIN(string $field, array $value,string $type = 'and') :self {
+         $inParama = [];
+         foreach ($value as $item){
+             $paramName = ':' . str_replace('.', '_', $field) . '_' . count($this->bindings);
+             $inParama[] = $paramName;
+             $this->bindings[$paramName] = $item;
+         }
+        $this->where[] =  sprintf(" %s %s IN ( %s ) ", (count($this->where) > 1) ? $type : '',$field, implode(' , ',$inParama));
         return $this;
     }
-    public function whereNOT_IN($field,$value,$type = 'and') {
-        if(is_array($value) && is_string($field)) {
-            $this->where[] =  sprintf("%s  %s NOT IN ( %s ) ", (count($this->where) > 1) ? $type : '',$field, implode(',',$value));
+    public function whereNOT_IN(string $field, array $value ,$type = 'and') :self {
+        $inParama = [];
+        foreach ($value as $item){
+            $paramName = ':' . str_replace('.', '_', $field) . '_' . count($this->bindings);
+            $inParama[] = $paramName;
+            $this->bindings[$paramName] = $item;
         }
+        $this->where[] =  sprintf(" %s %s NOT IN ( %s ) ", (count($this->where) > 1) ? $type : '',$field, implode(' , ',$inParama));
+        return $this;
     }
-    public function whereBetween($field,$value1,$value2,$type = 'and') {
-        if(is_string($field)) {
-            $this->where[] =  sprintf(" %s %s  BETWEEN  '%s' AND '%s' ", (count($this->where) > 1) ? $type : '',$field,$value1,$value2);
+    public function whereBetween(string $field,mixed $value1, mixed  $value2,string $type = 'and') :self {
+        $val1 =  ':' . str_replace('.', '_', $field) . '_b1_' . count($this->bindings);
+        $val2 =  ':' . str_replace('.', '_', $field) . '_b1_' . count($this->bindings);
+        $this->bindings[$val1] = $value1;
+        $this->bindings[$val2] = $value2;
+        $this->where[] =  sprintf(" %s  %s  BETWEEN  '%s' AND '%s' ",(count($this->where) > 1) ? $type : '', $field,$val1,$val2);
+        return $this;
+    }
+
+    public function whereNotBetween(string $field,mixed $value1, mixed  $value2,string $type = 'and') :self{
+        $val1 =  ':' . str_replace('.', '_', $field) . '_b1_' . count($this->bindings);
+        $val2 =  ':' . str_replace('.', '_', $field) . '_b1_' . count($this->bindings);
+        $this->bindings[$val1] = $value1;
+        $this->bindings[$val2] = $value2;
+        $this->where[] =  sprintf(" %s  %s  NOT BETWEEN  '%s' AND '%s' ",(count($this->where) > 1) ? $type : '', $field,$val1,$val2);
+        return $this;
+    }
+    public function whereRaw($sql,$type = 'and') :self{
+        if($sql  instanceof DbExceptionSQL) {
+            $this->where[] = ((count($this->where) > 1) ? $type : '').$sql->sql;
         }
         return $this;
     }
 
-    public function whereNotBetween($field,$value1,$value2,$type = 'and'){
-        if(is_string($field)) {
-            $this->where[] =  sprintf(" %s  %s  NOT BETWEEN  '%s' AND '%s' ",(count($this->where) > 1) ? $type : '', $field,$value1,$value2);
-        }
+    public function group(string|array $field) :self{
+        $this->group = (!is_array($field))?[$field]:$field;
         return $this;
     }
-    public function whereRaw($sql,$type = 'and'){
-        if(is_string($sql)) {
-            $this->where[] = ((count($this->where) > 1) ? $type : '').$sql;
-        }
+    public function having( string $field, mixed $value, string$operator = '=', string $type = 'and') :self{
+        $type = strtoupper($type);
+        $prefix = empty($this->where) ? '' : "{$type} ";
+        $paramName = ':' . str_replace('.', '_', $field) . '_' . count($this->bindings);
+        $this->bindings[$paramName] = $value;;
+        $this->having[] = "{$prefix}{$field} {$operator} {$paramName}";
         return $this;
     }
-
-    public function group($field){
-            if(!is_array($field))
-                $field = [$field];
-            $this->group = 'GROUP BY '.implode(' , ',$field);
-            return $this;
-    }
-    public function having($field,$value,$operator = '=',$type = 'and'){
-        if(is_string($field) && is_string($operator) && is_string($type)) {
-        $this->having[] = sprintf(" %s %s %s '%s' ",(count($this->having) > 1) ? $type : '', $field, $operator, $value );
-        }
-        return $this;
-    }
-    public function havingRaw($sql,$type = 'and'){
+    public function havingRaw(string $sql,$type = 'and') :self {
         if(is_string($sql)) {
             $this->having[] = ((count($this->having) > 1) ? $type : '').$sql;
         }
         return $this;
     }
 
-    public function order($field,$sort='asc'){
-        if(is_string($field) && is_string($sort))
-            $this->order[] = printf(" %s %s",$field,$sort);
+    public function order(string $field, string $sort = 'asc'):self {
+        $this->order[] = printf(" %s %s",$field,$sort);
         return $this;
     }
     public function limit($limit){
-        $this->limit = " LIMIT $limit ";
+        $this->limit = $limit;
         return $this;
     }
-    public function offset($offset) {
-        $this->offset = " OFFSET $offset ";
+    public function offset($offset):self {
+        $this->offset = $offset;
         return $this;
     }
-    public function get(){
+    public function get() : array {
+         $stmt =  $this->execute();
+         $result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+         $stmt = null;
+         return $result;
+    }
+    public function count() : int{
+        $temp = $this->select;
+        $this->select = ['COUNT(id) as \'count\''];
+        $stmt = $this->execute();
+        $result = $stmt->fetchColumn(0);
+        $this->select = $temp;
+        $stmt = null;
+        return (($result)??0);
+    }
 
 
+    protected function execute(): PDOStatement {
+       $this->getQueryString();
+       dd($this->strQusery);
+       $stmt  = self::$pdo->prepare($this->strQusery);
+       $stmt->execute($this->bindings);
+       return $stmt;
+    }
+    public function getQueryString(): self {
+        $selectField = empty($this->select)?'*':implode(' , ',$this->select);
+        $fromElement = empty($this->from)?'': implode(' , ',$this->from);;
+        $this->strQusery = "SELECT {$selectField} FROM {$fromElement}";
+        if(!empty($this->join))
+            $this->strQusery .= implode(' ',$this->join);
+        if(!empty($this->where))
+            $this->strQusery .= " WHERE ".implode(' ',$this->where);
+        if(!empty($this->group))
+            $this->strQusery .= " GROUP BY ".implode(' , ',$this->group);
+        if(!empty($this->having))
+            $this->strQusery .= " HAVING ".implode(' ',$this->having);
+        if(!empty($this->order))
+            $this->strQusery .= " ORDER BY ".implode(' , ',$this->order);
+        if(!empty($this->limit))
+            $this->strQusery .= " LIMIT ".$this->limit;
+        if (!empty($this->offset))
+            $this->strQusery .= " OFFSET ".$this->offset;
+        return $this;
+    }
+
+    public function insert( string $table,array $data): bool {
+        $field =  implode(' , ',array_keys($data)) ;
+        $pamBindings =  implode(' , ',array_map(function($k){return  ":$k" ;},array_keys($data)));
+        $this ->strQusery = "INSERT INTO {$table} ({$field}) VALUES ({$pamBindings})";
+        $stmt = self::$pdo->prepare($this ->strQusery);
+        $result = $stmt->execute($data);
+        $stmt = null;
+        return  $result;
+    }
+    public function insertGetIncriment(string $table,array $data):int|bool{
+        if($this->insert($table,$data))
+            return self::$pdo->lastInsertId();
+        return false;
 
     }
-    public function count(){}
 
-    public function insert($dataArr){
-
+    public function insertIgnore(string $table,array $data) {
+        return$this;
     }
-    public function insertGetIncriment($dataArr){}
-    public function insertMultiple($dataArr){}
+    public function insertOrUpdate(string $table,array $data): bool {
+        return$this;
+    }
+
+    public function insertMultiple(string $table,array $data) :bool {
+        if(!array_is_list($data))
+            throw new \Exception('Не индексированій массив');
+        $field =  implode(' , ',array_keys($data[0])) ;
+        $size = count($data);
+        $countField = count($data[0]);
+        $values = $params = [];
+        for ( $i = 0 ;$i< $size; $i++) {
+            $pamBindings = array_fill(0,$countField,'?');
+            $values[] = '('.implode(' , ',$pamBindings).')';
+            $params = array_merge($params,array_values($data[$i]));
+        }
+        $this ->strQusery = "INSERT INTO {$table} ({$field}) VALUES ".implode(',',$values);
+        $stmt = self::$pdo->prepare($this ->strQusery);
+        $result =  $stmt->execute($params);
+        $stmt = null;
+        return  $result;
+    }
 
     public function beginTransaction(){
-        $this->connection->begin_transaction();
+       self::$pdo->beginTransaction();
     }
     public function commit(){
-        $this->connection->commit();
+        self::$pdo->commit();
     }
 
     public function rollback(){
-        $this->connection->rollback();
+        self::$pdo->rollBack();
     }
 
-    public function update(){}
-    public function delete(){}
-    public function getQueryString(){}
-    public function clear(){}
-    public function execute(){}
+    public function update(string $table,array $data): bool {
+        $this ->strQusery = "UPDATE {$table} SET  ";
+        if(!empty($this->where))
+            $this ->strQusery .= "WHERE ".implode(' ', $this->where);
+        $stmt = self::$pdo->prepare($this ->strQusery);
+        $result =  $stmt->execute($this->bindings);
+        $stmt = null;
+        return  $result;
 
+    }
+    public function delete(string $table){
 
+        $this ->strQusery = "DELETE FROM {$table} ";
+        if(!empty($this->where))
+            $this ->strQusery .= "WHERE ".implode(' ', $this->where);
+        $stmt = self::$pdo->prepare($this ->strQusery);
+        $result =  $stmt->execute($this->bindings);
+        $stmt = null;
+        return  $result;
+    }
+
+    public function clear():self{
+            $this->select = [];
+            $this->from = [];
+            $this->join = [];
+            $this->where = [];
+            $this->group = [];
+            $this->having = [];
+            $this->order = [];
+            $this->limit = null;
+            $this->offset = null;
+            $this->bindings = [];
+            return $this;
+    }
 }
--- HTTP/Front/Controller.php
+++ HTTP/Front/Controller.php
@@ -1,13 +1,37 @@
 <?php
 namespace HTTP\Front;
 use App\Engine\BaseController;
+use App\Engine\DB;
+
 class Controller extends BaseController {
    public function callAction($action, $params){
             return parent::callAction($action, $params);
    }
 
     public function indexAction() {
-
+      $t =  new DB();
+      $t->insertMultiple('articl',[
+          ['name'=>'test1',
+          'text'=>'test1',
+          'description'=>'test1',
+              'updated_at'=>date('Y-m-d H:i:s'),],
+          ['name'=>'tes2',
+              'text'=>'tes2',
+              'description'=>'tes2',
+              'updated_at'=>date('Y-m-d H:i:s')],
+          ['name'=>'test3',
+              'text'=>'test3',
+              'description'=>'test3',
+              'updated_at'=>date('Y-m-d H:i:s')],
+          ['name'=>'test4',
+              'text'=>'test5',
+              'description'=>'test5',
+              'updated_at'=>date('Y-m-d H:i:s')],
+          ['name'=>'test6',
+              'text'=>'test7',
+              'description'=>'test8',
+              'updated_at'=>date('Y-m-d H:i:s')],
+        ]);
     }
     public function catalogAction() {
 
